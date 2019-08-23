860e66bbe976e06c754440cf9a710709c85d72bc
rodrigo <rodrigo@payaqui.com>
2019-08-22 23:51:27 -0300

    Exercicio de internacionalizacao

---
+++ app/src/main/java/com/abud/paisecomida/MainActivity.java
@@ -0,0 +1,15 @@
+package com.abud.paisecomida;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.abud.paisecomida.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
